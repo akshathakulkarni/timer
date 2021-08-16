8a5004ef975b8b1771afe691cf96e0e7e79ae2bf
Akshatha Kulkarni <akshathakulkarni12@gmail.com>
2021-08-16 14:45:47 -0400

    Refactor simple timer

--- timer1.js
+++ timer1.js
@@ -1,14 +1,18 @@
 const args = process.argv.slice(2);
-//console.log(args);
-let negativeNumberCheck = Math.sign(args);
-let numberCheck = args[0];
-if ((args.length === 0) || (negativeNumberCheck === -1) || (!(isNaN(numberCheck)) === false)) {
-  return;
-} else {
-  setTimeout(() => {
-    process.stdout.write('.');
-  }, args * 1000);
-  setTimeout(() => {
-    process.stdout.write('\n');
-  }, 0);
+
+const timer = args => {
+  for (const number of args) {
+    if (!isNaN(number) && (number > 0)) {
+      setTimeout(() => {
+        process.stdout.write(`${number} \n`);
+      }, number * 1000);
+    }
+  }
 }
+
+timer(args);
+
+//console.log(args);
+//let negativeNumberCheck = Math.sign(args);
+//let numberCheck = args[0];
+
